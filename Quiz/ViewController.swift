c72ccad2a509e0b773a199f221d33bb0ae282288
Daniel Barclay <barclaysd@me.com>
2019-06-02 15:10:00 +0100

    Implemented success message, feedback for users answers through use of ProgressHUD and dynamically sizing progress bar

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     var currentQuestion : Question!
     var questionNumber: Int = 0
     var score: Int = 0
+    var displayMessage: Array = [String]()
+    
+    // identify screen width to calculate progress bar increments
+    public var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -22,7 +28,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nextQuestion()
         updateUI()
     }
 
@@ -30,23 +35,42 @@ class ViewController: UIViewController {
     @IBAction func answerPressed(_ sender: AnyObject) {
         let userAnswer : Bool = answerOptions[sender.tag-1]
         checkAnswer(selectedAnswer: userAnswer)
+        questionNumber += 1
+        updateUI()
     }
     
     
     func updateUI() {
         scoreLabel.text = "Score: \(score)"
+        if questionNumber+1 <= allQuestions.questionList.count {
         progressLabel.text = "\(questionNumber+1)/\(allQuestions.questionList.count)"
+            
+        progressBar.frame.size.width = CGFloat((screenWidth/CGFloat(allQuestions.questionList.count)) * CGFloat(questionNumber+1))
+        }
+        
+        nextQuestion()
     }
     
 
     func nextQuestion() {
         
-        if questionNumber+1 < allQuestions.questionList.count {
+        if questionNumber+1 <= allQuestions.questionList.count {
             currentQuestion = allQuestions.questionList[questionNumber]
             questionLabel.text = currentQuestion.questionText
         } else {
             
-            let alert = UIAlertController(title: "Great Job!", message: "Congratulations, you've made it to the end of the quiz with a score of \(score).", preferredStyle: .alert)
+            let scorePercentage = (score * 200 + allQuestions.questionList.count)/(2 * allQuestions.questionList.count)
+            
+            
+            if scorePercentage > 80 {
+                setMessage(title: "Knocked it out of the park! 💯", message: "Awesome! What a showing that was... you really know your stuff 🤓 You finished with a score of \(score): \(scorePercentage)%")
+            } else if scorePercentage > 50 {
+                setMessage(title: "Not too shabby! 😏", message: "Nice one champ, keep learning to reach the top! You finished with a score of \(score): \(scorePercentage)%")
+            } else {
+                setMessage(title: "Poor show 😪", message: "Unlucky, better luck next time! 💪🏼 You finished with a score of \(score): \(scorePercentage)%")
+            }
+            
+            let alert = UIAlertController(title: displayMessage[0], message: displayMessage[1], preferredStyle: .alert)
             
             let restartAction = UIAlertAction(title: "Restart Quiz", style: .default) { (UIAlertAction) in
                 self.startOver()
@@ -63,11 +87,10 @@ class ViewController: UIViewController {
        
         if selectedAnswer == currentQuestion.answer {
             score += 1
+            ProgressHUD.showSuccess("Correct!")
+        } else {
+            ProgressHUD.showError("Wrong!")
         }
-        
-        questionNumber += 1
-        nextQuestion()
-        updateUI()
     }
     
     
@@ -75,7 +98,10 @@ class ViewController: UIViewController {
        questionNumber = 0
         score = 0
         updateUI()
-        nextQuestion()
+    }
+    
+    func setMessage(title: String, message: String) {
+        displayMessage = [title, message]
     }
     
 
